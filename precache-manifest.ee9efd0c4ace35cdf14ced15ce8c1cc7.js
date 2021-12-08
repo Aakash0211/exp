0cb3475c2c9102a8caaef0e713239d715de45577
Aakash <akashkushi111@gmail.com>
2021-12-08 11:38:17 +0530

    Updates

---
+++ precache-manifest.ee9efd0c4ace35cdf14ced15ce8c1cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2dd5faac61b6546fb21860a0a2410f5",
+    "url": "/exp/index.html"
+  },
+  {
+    "revision": "4a151890b15f0e0d0000",
+    "url": "/exp/static/css/main.408403e1.chunk.css"
+  },
+  {
+    "revision": "18a56a28f9031bd0a15e",
+    "url": "/exp/static/js/2.53fd96af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exp/static/js/2.53fd96af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a151890b15f0e0d0000",
+    "url": "/exp/static/js/main.2cd475ca.chunk.js"
+  },
+  {
+    "revision": "6c4eb32ab3fb48b29873",
+    "url": "/exp/static/js/runtime-main.1cbe9961.js"
+  }
+]);
